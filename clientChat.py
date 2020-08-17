1db4d7986223fd899cbc9a62fa58fbe8bda9b25a
toti <sam17332@uvg.edu.gt>
2020-08-16 21:12:37 -0600

    Cambios en chat.

--- clientChat.py
+++ clientChat.py
@@ -1,32 +1,18 @@
 import socket
-import select 
-import errno
 import sys
 
 HEADER_LENGTH = 10
 IP = "127.0.0.1"
 PORT = 1234
 
-mi_usuario = input("Usuario: ")
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((IP, PORT))
-client_socket.setblocking(False)
-
-usuario = mi_usuario.encode("utf-8")
-usuario_header = f"{len(usuario):<{HEADER_LENGTH}}".encode("UTF-8")
-client_socket.send(usuario_header + usuario)
-
-while True: 
-    mensaje = input(f"{mi_usuario} > ")
-
-    if mensaje: 
+def recibir_mensajes(mensaje):
+    if mensaje:
         mensaje = mensaje.encode('utf-8')
         mensaje_header = f"{len(mensaje):<{HEADER_LENGTH}}".encode('utf-8')
         client_socket.send(mensaje_header + mensaje)
 
     try: 
-        while True: 
-            #recibir
+        while True:
             usuario_header = client_socket.recv(HEADER_LENGTH)
             if not len(usuario_header):
                 print("Conexion cerrada por el servidor")
@@ -39,17 +25,53 @@ while True:
             mensaje_length = int(mensaje_header.decode('utf-8').strip())
             mensaje = client_socket.recv(mensaje_length).decode('utf-8')
 
-            print(f"{usuario} > {mensaje}")
+            usr_len = len(usuario)
+            ab2 = "-" * (usr_len + 4)
+
+            print(f"{ab2}")
+            print(f"| {usuario} | ")
+            print(f"{ab2}")
+            print(f"{mensaje}\n")
 
     except:
         pass
 
-    # except IOError as e:
-    #     if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-    #         print('Error de lectura', str(e))
-    #         sys.exit()
-    #     continue
+print("""
+ #####                                    #                                                                 
+#     #   ##   #####  #####   ####       # #    ####    ##   # #    #  ####  #####     
+#        #  #  #    # #    # #          #   #  #    #  #  #  # ##   # #        #         
+#       #    # #    # #    #  ####     #     # #      #    # # # #  #  ####    #         
+#       ###### #####  #    #      #    ####### #  ### ###### # #  # #      #   #      
+#     # #    # #   #  #    # #    #    #     # #    # #    # # #   ## #    #   #      
+ #####  #    # #    # #####   ####     #     #  ####  #    # # #    #  ####    #      
+                                      
+                                                                                                                                                                                     
+#     # #    # #    #   ##   #    # # ##### #   #     #####  #    #   ##   #####
+#     # #    # ##  ##  #  #  ##   # #   #    # #     #     # #    #  #  #    #
+####### #    # # ## # #    # # #  # #   #     #      #       ###### #    #   #
+#     # #    # #    # ###### #  # # #   #     #      #       #    # ######   #   
+#     # #    # #    # #    # #   ## #   #     #      #     # #    # #    #   #   
+#     #  ####  #    # #    # #    # #   #     #       #####  #    # #    #   #   
+""")
 
-    # except Exception as e:
-    #     print('Error', str(e))
-    #     sys.exit()
+mi_usuario = input("Usuario: ")
+mi_id = input("Usuario id: ")
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect((IP, PORT))
+client_socket.setblocking(False)
+
+usuario = mi_usuario.encode("utf-8")
+usuario_header = f"{len(usuario):<{HEADER_LENGTH}}".encode("UTF-8")
+
+id_usuario = mi_id.encode("utf-8")
+id_header = f"{len(id_usuario):<{HEADER_LENGTH}}".encode("UTF-8")
+
+client_socket.send(usuario_header + usuario) 
+
+while True: 
+    mensaje = input(f"{mi_usuario}: \n")
+    print("")
+    # time.sleep(3)
+
+    recibir_mensajes(mensaje)
+
